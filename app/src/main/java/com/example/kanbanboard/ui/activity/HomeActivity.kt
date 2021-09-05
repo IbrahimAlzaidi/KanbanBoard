e2fdaef052b46f56f7b305cc1b61dc1763c71e03
Nooralden <nooraldenakel@gmail.com>
2021-09-05 23:50:49 +0300

    enable the Bottom NavigationBar

--- app/src/main/java/com/example/kanbanboard/ui/activity/HomeActivity.kt
+++ app/src/main/java/com/example/kanbanboard/ui/activity/HomeActivity.kt
@@ -3,6 +3,7 @@ package com.example.kanbanboard.ui.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import com.example.kanbanboard.R
 import com.example.kanbanboard.databinding.ActivityHomeBinding
 import com.example.kanbanboard.ui.fragments.HomeFragment
 import com.example.kanbanboard.ui.fragments.ProfileFragment
@@ -19,24 +20,22 @@ class HomeActivity : AppCompatActivity() {
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-//        addNavigationListener()
+        addNavigationListener()
     }
 
-//    private fun addNavigationListener() {
-//        binding.navigationBar.setOnItemSelectedListener { item ->
-//            replaceFragment(
-//                when(item.itemId){
-//                    R.id.nav_home -> fragmentHome
-//                    R.id.nav_info -> fragmentTaskStats
-//                    R.id.nav_search -> fragmentProfile
-//                    R.id.nav_details -> fragmentDetails
-//                    R.id.nav_vaccination_daily_info -> fragmentVaccination
-//                    else -> return@setOnItemSelectedListener  false
-//                }
-//            )
-//            return@setOnItemSelectedListener true
-//        }
-//    }
+    private fun addNavigationListener() {
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            replaceFragment(
+                when(item.itemId){
+                    R.id.homePage -> fragmentHome
+                    R.id.statisticPage -> fragmentTaskStats
+                    R.id.profilePage -> fragmentProfile
+                    else -> return@setOnItemSelectedListener  false
+                }
+            )
+            return@setOnItemSelectedListener true
+        }
+    }
 
     private fun replaceFragment(newFragment: Fragment){
         val transaction = supportFragmentManager.beginTransaction()
