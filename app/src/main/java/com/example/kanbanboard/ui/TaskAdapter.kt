2762ba086743905865374a8e0feaa5f8c0ece2e3
Rafal A <rafal.akeel99@gmail.com>
2021-09-10 04:44:51 +0300

    fix margin of home fragment and add functionality to user icon

--- app/src/main/java/com/example/kanbanboard/ui/TaskAdapter.kt
+++ app/src/main/java/com/example/kanbanboard/ui/TaskAdapter.kt
@@ -1,16 +1,21 @@
 package com.example.kanbanboard.ui
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.kanbanboard.R
+import com.example.kanbanboard.data.DbHelper
 import com.example.kanbanboard.databinding.ItemTaskBinding
 import com.example.kanbanboard.model.DbTaskModel
+import org.w3c.dom.Text
 
 class TaskAdapter(private val list:MutableList <DbTaskModel>):RecyclerView.Adapter<TaskAdapter.TaskViewHolder>()
 {
-
+val dbHelper:DbHelper? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_task,parent,false)
@@ -18,11 +23,12 @@ class TaskAdapter(private val list:MutableList <DbTaskModel>):RecyclerView.Adapt
     }
 
     override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
-        val  currentPosition = list[position]
+        val currentPosition = list[position]
         holder.binding.apply {
-            textTaskTitle.text =currentPosition.titleTask
+            textTaskTitle.text = currentPosition.titleTask
             textTaskDescription.text = currentPosition.descTask
             taskStatus.text = currentPosition.statsTask
+            userText.text = currentPosition.userName.subSequence(0,2)
         }
     }
 
