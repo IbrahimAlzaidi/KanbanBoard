56693d7732d3943eef8c7d2214085b32cae16ab7
Naufal Aliraqi <aliraqixnaufal1998@gmail.com>
2021-09-10 14:41:52 +0300

    last update profile fragment

--- app/src/main/java/com/example/kanbanboard/ui/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/kanbanboard/ui/fragments/ProfileFragment.kt
@@ -1,15 +1,23 @@
 package com.example.kanbanboard.ui.fragments
 
 import android.view.LayoutInflater
+import com.example.kanbanboard.data.DbHelper
 import com.example.kanbanboard.databinding.FragmentProfileBinding
+import com.example.kanbanboard.ui.TaskAdapter
+import com.example.kanbanboard.ui.TaskAdapterProfile
 
 class ProfileFragment:BaseFragment<FragmentProfileBinding>() {
     override val LOG_TAG: String = "Profile Fragment"
+    val daily = "4"
+    val weekly = "18"
     override val bindingInflater: (LayoutInflater) -> FragmentProfileBinding
         get() = FragmentProfileBinding::inflate
 
     override fun setup() {
-
+        binding?.textDaily?.text = daily
+        binding?.textWeekly?.text = weekly
+        val adapter = TaskAdapterProfile(DbHelper(requireContext()).getAllTasksByUser(binding?.textUserName?.text.toString()))
+        binding?.taskRecyclerView?.adapter = adapter
     }
 
     override fun addCallBack() {
---
+++ app/src/main/java/com/example/kanbanboard/ui/fragments/TaskAdapterProfile.kt
@@ -0,0 +1,42 @@
+package com.example.kanbanboard.ui
+
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.kanbanboard.R
+import com.example.kanbanboard.data.DbHelper
+import com.example.kanbanboard.databinding.ItemTaskBinding
+import com.example.kanbanboard.model.DbTaskModel
+import com.example.kanbanboard.util.ClickListener
+import org.w3c.dom.Text
+import java.security.PrivateKey
+
+class TaskAdapterProfile(private val list:MutableList <DbTaskModel>):RecyclerView.Adapter<TaskAdapterProfile.TaskViewHolder>() {
+    val dbHelper: DbHelper? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_task, parent, false)
+        return TaskViewHolder(view)
+    }
+
+    override fun getItemCount() = list.size
+
+    class TaskViewHolder(viewItem: View) : RecyclerView.ViewHolder(viewItem) {
+        val binding = ItemTaskBinding.bind(viewItem)
+    }
+
+    override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
+        val currentPosition = list[position]
+        holder.binding.apply {
+            textTaskTitle.text = currentPosition.titleTask
+            textTaskDescription.text = currentPosition.descTask
+            taskStatus.text = currentPosition.statsTask
+            userText.text = currentPosition.userName.subSequence(0, 2)
+            taskDate.text = currentPosition.dateTask
+        }
+    }
+}
